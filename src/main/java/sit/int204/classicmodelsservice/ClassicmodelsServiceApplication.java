98c15a95e688736c32c93ed90bfe12f561cd858a
BomScoob <sorawitkraukham@gmail.com>
2024-01-16 14:08:15 +0700

    Initial commit

---
+++ src/main/java/sit/int204/classicmodelsservice/ClassicmodelsServiceApplication.java
@@ -0,0 +1,13 @@
+package sit.int204.classicmodelsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicmodelsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassicmodelsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sit/int204/classicmodelsservice/ClassicmodelsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sit.int204.classicmodelsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicmodelsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
