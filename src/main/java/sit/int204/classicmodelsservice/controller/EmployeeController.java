e1643237d1c33fa4a452a5da5331418e230c6cfb
BomScoob <sorawitkraukham@gmail.com>
2024-01-20 14:26:32 +0700

    EmployeeController, implement basic request endpoint.

---
+++ src/main/java/sit/int204/classicmodelsservice/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package sit.int204.classicmodelsservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import sit.int204.classicmodelsservice.entities.Employee;
+import sit.int204.classicmodelsservice.services.EmployeeService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    @Autowired
+    private EmployeeService service;
+
+    @GetMapping("")
+    public List<Employee> getAllEmployees() {
+        return service.getAllEmployees();
+    }
+
+    @PostMapping("")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        return service.createNewEmployee(employee);
+    }
+
+    @PutMapping("/{empNumber}")
+    public Employee updateEmployee(@RequestBody Employee employee, @PathVariable Integer empNumber) {
+        return service.updateEmployee(empNumber, employee);
+    }
+
+    @DeleteMapping("/{empNumber}")
+    public void removeEmployee(@PathVariable Integer empNumber){
+        service.removeEmployee(empNumber);
+    }
+}
