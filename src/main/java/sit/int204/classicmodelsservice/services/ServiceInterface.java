40271353f6c3e8d1cc7ee9a04657944c65a5224b
BomScoob <sorawitkraukham@gmail.com>
2024-01-30 17:15:46 +0700

    clean code and import

--- src/main/java/sit/int204/classicmodelsservice/services/ServiceInterface.java
+++ src/main/java/sit/int204/classicmodelsservice/services/ServiceInterface.java
@@ -1,11 +1,8 @@
 package sit.int204.classicmodelsservice.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import sit.int204.classicmodelsservice.repositories.OrderRepository;
-
 import java.util.List;
 
 public interface ServiceInterface <ENTITY, KEY_TYPE> {
-    public List<ENTITY> getAllEntities();
-    public ENTITY getEntity(KEY_TYPE id);
+    List<ENTITY> getAllEntities();
+    ENTITY getEntity(KEY_TYPE id);
 }
