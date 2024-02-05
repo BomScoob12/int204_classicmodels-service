fcf224d505364518f979cec173dca828eb4a7d2d
BomScoob <sorawitkraukham@gmail.com>
2024-02-05 18:22:08 +0700

    Update : GenericController and ServiceInterface adding necessary method and endpoint into GenericController

--- src/main/java/sit/int204/classicmodelsservice/controller/GenericController.java
+++ src/main/java/sit/int204/classicmodelsservice/controller/GenericController.java
@@ -1,6 +1,7 @@
 package sit.int204.classicmodelsservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 import sit.int204.classicmodelsservice.services.ServiceInterface;
 
@@ -20,18 +21,18 @@ public abstract class GenericController <S extends ServiceInterface<ENTITY, KEY_
         return service.getEntity(id);
     }
 
-//    @PostMapping("")
-//    public ENTITY addNewEntity(@RequestBody ENTITY entity) {
-//        return service.createNewEntity(entity);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public ENTITY updateEntity(@RequestBody ENTITY entity, @PathVariable KEY_TYPE id) {
-//        return service.updateEntity(id, entity);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public void removeEntity(@PathVariable KEY_TYPE id) {
-//        service.removeEntity(id);
-//    }
+    @PostMapping("")
+    public ENTITY addNewEntity(@RequestBody ENTITY entity) {
+        return service.createNewEntity(entity);
+    }
+
+    @PutMapping("/{id}")
+    public ENTITY updateEntity(@PathVariable KEY_TYPE id, @RequestBody ENTITY entity) {
+        return service.updateEntity(id, entity);
+    }
+
+    @DeleteMapping("/{id}")
+    public void removeEntity(@PathVariable KEY_TYPE id) {
+        service.removeEntity(id);
+    }
 }
--- src/main/java/sit/int204/classicmodelsservice/services/ServiceInterface.java
+++ src/main/java/sit/int204/classicmodelsservice/services/ServiceInterface.java
@@ -1,8 +1,14 @@
 package sit.int204.classicmodelsservice.services;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 public interface ServiceInterface <ENTITY, KEY_TYPE> {
     List<ENTITY> getAllEntities();
     ENTITY getEntity(KEY_TYPE id);
+    ENTITY createNewEntity(ENTITY entity);
+    ENTITY updateEntity(KEY_TYPE id ,ENTITY entity);
+    void removeEntity(KEY_TYPE id);
 }
